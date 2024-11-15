7dd9163d932df928a29bdb9d40f724f23528eb3d
Michele Mandanici <michimandanici@tim.it>
2024-11-15 10:02:34 +0100

    edit

--- index.js
+++ index.js
@@ -9,50 +9,69 @@ const imgLoadSecondButton = document.getElementById('imgLoadSecond')
 const searchImgInput = document.getElementById('searchImg')
 const albumContainer = document.querySelector('.album .container')
 
-fetch('https://api.pexels.com/v1/search?query=cars&per_page=10', {
-  headers: {
-    Authorization: apiKey,
-  },
-})
-  .then((response) => {
-    if (response.ok) {
-      // qui finiamo se la chiamata è 100% corretta e possiamo recuperare il JSON
-      // dei risultati
-      console.log('Ho preso i dati')
-      return response.json()
-    } else {
-      // qui finiamo ad esempio se l'endpoint non esiste, e in generale se la
-      // Response è arrivata ma non contiene i dati per proseguire
-      throw new Error('Errore nel recupero della risposta dal server')
-    }
+const getImages = function () {
+  fetch('https://api.pexels.com/v1/search?query=cars', {
+    headers: {
+      Authorization: apiKey,
+    },
   })
-  .then((data) => {
-    albumContainer.innerHTML = '' // Svuota la sezione album prima di inserire nuove immagini
-    data.photos.forEach((photo) => {
-      const card = document.createElement('div')
-      card.classList.add('col', 'col-12', 'col-md-4')
-      card.innerHTML = `
-        <div class="card mb-4 shadow-sm">
-          <img src="${photo.src.medium}" class=" card-img-top" alt="${photo.alt}" />
-          <div class="card-body">
-            <h5 class="card-title">
-              <a href="detail.html?id=${photo.id}" target="_blank">${photo.alt}</a>
-            </h5>
-            <p class="card-text">This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary hide-btn">Hide</button>
-              </div>
-              <small class="text-muted">${photo.id}</small>
-            </div>
-          </div>
-        </div>
-      `
-      // Aggiungi event listener per pulsante "Hide"
-      card
-        .querySelector('.hide-btn')
-        .addEventListener('click', () => card.remove())
-      albumContainer.appendChild(card)
+    .then((response) => {
+      if (response.ok) {
+        // qui finiamo se la chiamata è 100% corretta e possiamo recuperare il JSON
+        // dei risultati
+        console.log('Ho preso i dati')
+        return response.json()
+      } else {
+        // qui finiamo ad esempio se l'endpoint non esiste, e in generale se la
+        // Response è arrivata ma non contiene i dati per proseguire
+        throw new Error('Errore nel recupero della risposta dal server')
+      }
     })
-  })
-  .catch((error) => console.error('Errore:', error))
+
+    .then((oggettone) => {
+      console.log(oggettone)
+    })
+    .catch((err) => {
+      console.log('ERRORE', err)
+    })
+}
+
+imgLoadButton.addEventListener('click')
+
+//   .then((data) => {
+//     albumContainer.innerHTML = '' // Svuota la sezione album prima di inserire nuove immagini
+//     data.photos.forEach((photo) => {
+//       const card = document.createElement('div')
+//       card.classList.add('col', 'col-12', 'col-md-6', 'col-lg-4')
+//       card.innerHTML = `
+//         <div class="card mb-4 shadow-sm">
+//           <img src="${photo.src.medium}" class="card-img-top" alt="${photo.alt}" />
+//           <div class="card-body">
+//             <h5 class="card-title">
+//               <a href="detail.html?id=${photo.id}" target="_blank">${photo.alt}</a>
+//             </h5>
+//             <p class="card-text">This content is a little bit longer.</p>
+//             <div class="d-flex justify-content-between align-items-center">
+//               <div class="btn-group">
+//                 <button type="button" class="btn btn-sm btn-outline-secondary hide-btn">Hide</button>
+//               </div>
+//               <small class="text-muted">${photo.id}</small>
+//             </div>
+//           </div>
+//         </div>
+//       `
+//       // Aggiungi event listener per pulsante "Hide"
+//       card
+//         .querySelector('.hide-btn')
+//         .addEventListener('click', () => card.remove())
+//       albumContainer.appendChild(card)
+//     })
+//   })
+//   .catch((error) => console.error('Errore:', error))
+
+// // Event listener per "Load Images" con query da campo di ricerca
+// imgLoadButton.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   const query = searchQueryInput.value || 'cars'
+//   loadImages(query)
+// })
